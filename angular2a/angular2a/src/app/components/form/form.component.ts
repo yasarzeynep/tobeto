05230d6a10fe96ec0497cdb6718014af813df462
Zeynep Yaşar <zynpsema@gmail.com>
2024-03-08 18:30:50 +0300

    add lesson 33

---
+++ angular2a/angular2a/src/app/components/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-form', //htmlde cagısralacak isim
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+/*ödev
+ngModel
+typescript interface/modeller
+two way data binding*/
+}
