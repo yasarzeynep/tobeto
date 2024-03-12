acb6345dd17afe13ca2008fa38adaa41e5fdbfa7
Zeynep Yaşar <zynpsema@gmail.com>
2024-03-12 12:45:22 +0300

    add lesson34
    promise

--- angular2a/src/app/pages/homepage/homepage.component.ts
+++ angular2a/src/app/pages/homepage/homepage.component.ts
@@ -1,14 +1,29 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 
 export class HomepageComponent {
+  constructor(private httpClient:HttpClient) {
+
+  }
   todoList: string[] = ['Eleman 1'];
+
+  ngOnInit()
+  {
+    console.log('ngOnInıt calıstı.');
+  }
+  getTodos()
+{
+//Backende istek atıp, verileri çek
+//Component Lifecycle
+}
+  
 }
